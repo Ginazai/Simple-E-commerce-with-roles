e618e6a99e3c748e83e691dad385753d1c706e7a
Ginazai <rafaeldc1300@gmail.com>
2024-04-23 16:30:25 -0500

    updated data model

--- registro.sql
+++ registro.sql
@@ -1,163 +1,198 @@
-create database myapp;
-use myapp;
----------------------------------------------
---Base
----------------------------------------------
-create table user(
-	id int auto_increment primary key not null,
-	fullname varchar(500) not null,
-	username varchar(100) not null unique,
-	email varchar(255) not null unique,
-	password varchar(255) not null,
-	created_at datetime not null
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
----------------------------------------------
-CREATE TABLE users_roles(
-	id int primary key auto_increment not null,
-	username varchar(100),
-	admin boolean NOT NULL,
-	customer boolean NOT NULL,
-	supervisor boolean NOT NULL,
-	hardware_specialist boolean NOT NULL,
-	software_specialist boolean NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE users_roles ADD UNIQUE INDEX(username);
---For inserting all the existing user usernames
-INSERT INTO users_roles(username)
-SELECT username FROM user;
---------------------------------------------
-CREATE TABLE roles(
-	id int not null auto_increment primary key,
-	role varchar(100)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
----------------------------------------------
+create database e_commerce_admin_module;
+use e_commerce_admin_module;
 create table permissions(
-	id int primary key auto_increment not null, 
+	permission_id int auto_increment primary key not null,
 	_read boolean not null,
 	_write boolean not null
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
----------------------------------------------
-create table user_roles(
-	id int auto_increment primary key not null, 
-	user_id int,
-	role_id int,
+create table roles(
+	role_id int auto_increment primary key not null,
+	role varchar(50) not null,
+	permission int not null,
 
-	CONSTRAINT fkuser_id
-		FOREIGN KEY (user_id)
-		REFERENCES user (id)
-		ON DELETE CASCADE,
-
-	CONSTRAINT fkrole_id
-		FOREIGN KEY (role_id)
-		REFERENCES roles (id)
-		ON DELETE CASCADE
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
----------------------------------------------
-create table role_permissions(
-	id int primary key auto_increment not null,
+	constraint fk_permission_role
+	foreign key (permission)
+	references permissions (permission_id)
+	on update cascade
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table users(
+	user_id int auto_increment primary key not null,
+	email varchar(100) not null,
+	password varchar(30) not null,
+	name varchar(50) not null,
+	last_name varchar(100) not null,
+	dob date not null,
+	phone_number varchar(9) not null,
+	active boolean not null, 
 	role_id int not null,
-	permission_id int not null,
 
-	constraint fk_role_id
+	constraint fk_role_id_user
 	foreign key (role_id)
-	references roles (id)
-	on delete cascade,
+	references roles (role_id)
+	on update cascade
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table provinces(
+	province_id int auto_increment primary key not null,
+	province varchar(15) not null
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table districts(
+	district_id int auto_increment primary key not null,
+	district varchar(50) not null,
+	province_id int not null,
 
-	constraint fk_permission_id
-	foreign key (permission_id)
-	references permissions (id)
-	on delete cascade
+	constraint fk_province_id_districts
+	foreign key (province_id)
+	references provinces (province_id)
+	on update cascade 
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
----------------------------------------------
---Audit
----------------------------------------------
-CREATE TABLE session(
-	id int not null auto_increment primary key,
-	username varchar(100) not null,
-	is_active boolean not null,
-	last_activity datetime not null
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
----------------------------------------------
-ALTER TABLE session ADD INDEX(username);
-CREATE TABLE data_trace(
-	id int AUTO_INCREMENT PRIMARY key not null, 
-    username varchar(100) not null,
-    _before varchar(150) not null, 
-    _after varchar(150) not null,
-    _date datetime not null
-)ENGINE=INNODB DEFAULT CHARSET=utf8;
----------------------------------------------
---Catalogs
----------------------------------------------
-CREATE TABLE categorias(
-	id int auto_increment primary key not null,
-	categoria varchar(50) not null
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
----------------------------------------------
-CREATE TABLE tickets(
-	id int auto_increment primary key not null,
-	nombre varchar(30) not null,
-	apellido varchar(30) not null,
-	email varchar(50) not null,
-	ticket varchar(500) not null,
-	consulta varchar(255) not null,
-	created_at timestamp not null,
-	updated_at timestamp not null
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
----------------------------------------------
-CREATE TABLE data_sales(
-	id_compra int primary key AUTO_INCREMENT not null,
-    titulo varchar(300) not null, 
-    descripcion varchar(300),
-    precio float not null,
-    username varchar(30) not null,
-    fechacompra date not null,
-    cantidad int not null
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
----------------------------------------------
-CREATE TABLE orders(
-	id_orden varchar(50) not null,
-	id_item int auto_increment primary key not null,
-	id_prod int not null,
+create table corregimientos(
+	corregimiento_id int auto_increment primary key not null,
+	corregimiento varchar(100) not null,
+	district_id int not null,
 
-	constraint fk_id_prod_order
-	foreign key (id_prod)
-	references data_sales (id_compra)
+	constraint fk_district_id_corregimientos
+	foreign key (district_id)
+	references districts (district_id)
+	on update cascade
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table address(
+	address_id int auto_increment primary key not null,
+	line_1 varchar(100) not null,
+	line_2 varchar(100),
+	corregimiento_id int not null,
+
+	constraint fk_corregimiento_id_address
+	foreign key (corregimiento_id)
+	references corregimientos (corregimiento_id)
+	on update cascade
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table user_addresses(
+	id int auto_increment primary key not null,
+	user_id int not null,
+	address_id int not null,
+	preferred boolean not null,
+
+	constraint fk_user_id_address
+	foreign key (user_id)
+	references users (user_id)
 	on delete cascade
 	on update cascade,
 
-	cantidad int not null,
-    username varchar(30) not null,
-    fechacompra date not null
+	constraint fk_address_id_user
+	foreign key (address_id)
+	references address (address_id)
+	on update cascade
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
----------------------------------------------
-CREATE TABLE receipt_list(
-	id_list varchar(50) not null,
-	id_prod int not null,
+create table inventory(
+	product_id int auto_increment primary key not null,
+	image varchar(65535),
+	product varchar(100) not null,
+	description varchar(300),
+	price float not null,
+	amount int not null
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table cart(
+	cart_id int auto_increment primary key not null,
+	product_id int not null,
+	user_id int not null,
+	amount int not null,
 
-	constraint fk_id_list_receipt
-	foreign key (id_list)
-	references receipt (id)
+	constraint fk_product_id_cart
+	foreign key (product_id)
+	references inventory (product_id)
 	on delete cascade
 	on update cascade,
 
-	constraint fk_id_prod_receipt
-	foreign key (id_prod)
-	references data_sales (id_compra)
+	constraint fk_user_id_cart
+	foreign key (user_id)
+	references users (user_id)
 	on delete cascade
+	on update cascade 
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table receipt(
+	receipt_id int auto_increment primary key not null,
+	user_id int not null, 
+	r_time timestamp not null,
+
+	constraint fk_user_id_receipt
+	foreign key (user_id)
+	references users (user_id)
+	on delete cascade 
+	on update cascade
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table shipping(
+	shipping_id int auto_increment primary key not null,
+	user_addresses_id int not null,
+	receipt_id int not null,
+	delivered boolean not null,
+
+	constraint fk_user_addresses_id_shipping
+	foreign key (user_addresses_id)
+	references user_addresses (id)
 	on update cascade,
 
-    precio float not null, 
-    cantidad int not null,
-    username varchar(30) not null
+	constraint fk_receipt_id_shipping
+	foreign key (receipt_id)
+	references receipt (receipt_id)
+	on delete cascade 
+	on update cascade
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
----------------------------------------------
-CREATE TABLE receipt(
+create table order_details(
 	id int auto_increment primary key not null,
-	id_list varchar(50) not null,
-    monto float not null,
-    username varchar(30) not null,
-    fechacompra timestamp not null
+	receipt_id int not null,
+	product_id int not null,
+	amount int not null,
+
+	constraint fk_receipt_id_order
+	foreign key (receipt_id)
+	references receipt (receipt_id)
+	on delete cascade 
+	on update cascade,
+
+	constraint fk_product_id_order
+	foreign key (product_id)
+	references inventory (product_id)
+	on update cascade
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table claims(
+	claim_id int auto_increment primary key not null,
+	user_id int not null,
+	receipt_id int not null,
+	issue varchar(500) not null,
+	solve boolean not null,
+
+	constraint fk_user_id_claims
+	foreign key (user_id)
+	references users (user_id)
+	on update cascade,
+
+	constraint fk_receipt_id_claims
+	foreign key (receipt_id)
+	references receipt (receipt_id)
+	on update cascade
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table data_trace(
+	trace_id int auto_increment primary key not null,
+	user_id int not null, 
+	data_before varchar(250) not null,
+	data_after varchar(250) not null,
+	_time timestamp not null,
+
+	constraint fk_user_id_data
+	foreign key (user_id)
+	references users (user_id)
+	on delete cascade 
+	on update cascade
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table data_session(
+	session_id int auto_increment primary key not null,
+	user_id int not null,
+	activity boolean not null,
+	_time timestamp not null,
+
+	constraint fk_user_id_data_session
+	foreign key (user_id)
+	references users (user_id)
+	on delete cascade
+	on update cascade
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
